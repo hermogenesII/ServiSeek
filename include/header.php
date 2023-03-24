d56d6933bf23fc5a7be818f4422756554d84a6f6
Fvck 0ff <hermogenes.magsino2@gmail.com>
2023-03-24 14:46:35 +0800

    add include folder and header.php

---
+++ include/header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <form class="d-flex me-auto">
+      <i id="search-btn" class="fas fa-magnifying-glass"></i>
+        <input class="form-control me-2" type="text" placeholder="Search">
+        <!-- <button class="btn btn-primary" type="button">Search</button> -->
+      </form>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
