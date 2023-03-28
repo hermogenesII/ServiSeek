e6e173ea271e797c795548ae96a98a197adf4a0d
Fvck 0ff <hermogenes.magsino2@gmail.com>
2023-03-28 20:23:14 +0800

    Add Navigational Bar

--- include/header.php
+++ include/header.php
@@ -1,24 +1,29 @@
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+    <a class="navbar-brand" href="javascript:void(0)"><img class="ms-5" src="./img/logo/logo.webp" alt="Logo" height="50"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="mynavbar">
-      <form class="d-flex me-auto">
-      <i id="search-btn" class="fas fa-magnifying-glass"></i>
-        <input class="form-control me-2" type="text" placeholder="Search">
-        <!-- <button class="btn btn-primary" type="button">Search</button> -->
+      <form class="navbar-form me-auto">
+        <input id="search-area" class="form-control me-2 :hover{width:500px;}" type="text" placeholder="Search...">
       </form>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+      <ul class="navbar-nav me-5">
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="javascript:void(0)"><i class="fa-solid fa-home"></i> Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="javascript:void(0)"><i class="fa-solid fa-map"></i> Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="javascript:void(0)"> <i class="fa-solid fa-message"></i> Messages</a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link" href="javascript:void(0)"> <i class="fa-solid fa-bell"></i> </a>
+        </li>
+        <li class="nav-item mx-3">
+          <a class="nav-link d-inline" href="javascript:void(0)">Hermo</a>
+          <img src="./img/profile/default.png" alt="profile" class="rounded-circle img-fluid" width="40px">
         </li>
       </ul>
     </div>
--- index.php
+++ index.php
@@ -2,20 +2,43 @@
 <html lang="en">
 
 <head>
-  <title>Servi-Seek Bootstrap</title>
+  <title>ServiSeek</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+  <style>
+    #search-area {
+      width: 221px;
+      box-sizing: border-box;
+      border: 2px solid #ccc;
+      border-radius: 4px;
+      font-size: 14px;
+      background-color: white;
+      background-image: url('./img/logo/search-icon.png');
+      background-position: 10px 10px;
+      background-repeat: no-repeat;
+      padding: 12px 20px 12px 40px;
+      -webkit-transition: width 0.4s ease-in-out;
+      transition: width 0.4s ease-in-out;
+    }
+
+    #search-area:focus {
+      width: 100%;
+    }
+  </style>
 
 </head>
 
 <body>
-<?php
+  <?php
 include_once './include/header.php';
 ?>
+
+
 
 </body>
 
